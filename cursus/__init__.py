22f7a280dc63685aa772ba5119ac2652cb852e5d
Richard Nguyen <richard@richardhnguyen.com>
2023-10-28 12:20:12 -0700

    feat: set up basic  configuration from environment variables

--- cursus/__init__.py
+++ cursus/__init__.py
@@ -1,17 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""OS module providing functionalities to work with operating system"""
 import os
 
 import flask
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app() -> Flask:
+    """App factory function to create a Flask app instance
+
+    The pattern is carefully described in the Flask documentation
+
+    :see https://flask.palletsprojects.com/en/3.0.x/patterns/appfactories/
+    """
+
     # Create a Flask application
     app = Flask(__name__, instance_relative_config=True)
 
     # Load configuration for the application
+    app.config.from_object(os.environ.get("APP_SETTINGS"))
 
     @app.route("/")
     def hello():
         return flask.jsonify({"message": "Hello, World!"})
+
+    @app.route("/config")
+    def config():
+        return flask.jsonify({"message": app.config["DATABASE_URL"]})
 
     return app
---
+++ cursus/config.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""OS module providing functionalities to work with operating system"""
+import os
+
+
+# from dotenv import load_dotenv
+
+# We don't need to load the .env file here manually
+
+
+class Config(object):
+    """Base configuration class for the application
+
+    This class contains all the default configuration variables that are used
+    accross the stages. The environment variables are loaded from the root
+    .env file by Flask itself (with `python-dotenv` installed).
+
+    """
+
+    TESTING = False
+    DEBUG = False
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    """Development configuration class for the application
+
+    This class contains all the development configuration variables that are
+    used during development. It inherits from the base configuration class and
+    overrides some variables to suit the development environment.
+
+    """
+
+    DEBUG = True
