5651ce85e503416f51b429c926fbfe0b6476b40c
Richard Nguyen <richard@richardhnguyen.com>
2023-11-14 11:01:03 -0800

    feat: add dropdown for navbar on mobile

---
+++ cursus/static/js/dropdown.js
@@ -0,0 +1,40 @@
+/**
+ * Toggle the dropdown menu with the given id.
+ *
+ * @param {string} dropdownId
+ */
+function toggleDropdown(dropdownId) {
+  let dropdown = document.getElementById(dropdownId);
+  let dropdownMenu = dropdown.querySelector(".dropdown__menu");
+
+  console.log(dropdownMenu);
+
+  dropdown.setAttribute(
+    "aria-expanded",
+    dropdownMenu.classList.contains("dropdown--show") ? "false" : "true"
+  );
+
+  dropdownMenu.classList.toggle("dropdown--show");
+}
+
+/**
+ * Close all dropdown menus when clicking outside of them.
+ *
+ * @param {Event} e
+ */
+function handleClickOutside(event) {
+  // Handle closing of dropdown menus when clicking outside
+  if (!event.target.matches(".dropdown__btn")) {
+    console.log(event.target);
+    const dropdowns = document.getElementsByClassName("dropdown__menu");
+
+    for (let dropdown of dropdowns) {
+      if (dropdown.classList.contains("dropdown--show")) {
+        dropdown.classList.remove("dropdown--show");
+
+        const parent = dropdown.parentElement;
+        parent.setAttribute("aria-expanded", "false");
+      }
+    }
+  }
+}
---
+++ cursus/static/js/index.js
@@ -0,0 +1,15 @@
+const navbarDropdown = document.getElementById("navbarDropdown");
+const navbarDropdownBtn = document.querySelector(".dropdown__btn");
+
+navbarDropdownBtn.addEventListener("click", function () {
+  toggleDropdown("navbarDropdown");
+});
+
+/**
+ * Handle global click events.
+ *
+ * @param {Event} event
+ */
+window.onclick = function (event) {
+  handleClickOutside(event); // from dropdown.js
+};
